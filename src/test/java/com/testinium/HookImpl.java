627fc76ba929cd0cd5dfc8fec35e2793fec7f00a
ebkaa <ebkaucsu@gmail.com>
2024-11-19 12:37:27 +0300

    Update HookImpl.java

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -104,7 +104,7 @@ public class HookImpl {
             if (System.getenv("platform").equals("ANDROID")) {
                 isDeviceAnd=true;
                 capabilities.setCapability("key", System.getenv("key"));
-                //capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.gratis.android");
+                capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.gratis.android");
                 capabilities.setCapability("appWaitActivity", ".activities.MainActivity");
                 capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.app.gratis.ui.splash.SplashActivity");
                 capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
